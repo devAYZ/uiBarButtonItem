ed74b7eff697b34d5300da9f4f8a39773ee287b2
devAYZ <fatokimiemman@gmail.com>
2022-01-10 11:10:39 +0100

    add xcode project

---
+++ barItem/barItem/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  barItem
+//
+//  Created by Ayokunle Pro on 1/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
